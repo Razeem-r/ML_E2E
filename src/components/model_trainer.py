650b9c82cfe7e13a0ffff7cf683e1f5833d87795
Razeem-r <kyuubiraz2@gmail.com>
2024-01-14 23:14:20 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,7 +37,36 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            models_report:dict = evaluate_models(X_train,y_train,X_test,y_test,models)
+            hyper_params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            models_report:dict = evaluate_models(X_train,y_train,X_test,y_test,models,hyper_params)
 
             best_model_score = max(sorted(models_report.values()))
             best_model_name = [key for key, value in models_report.items() if value == best_model_score][0]
